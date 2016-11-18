24a06bd648f273c0319741004e6d20b5673d71a5
Ricardo Gobbo <ricardo@clicknow.com.br>
2016-11-18 19:46:21 -0200

    Add more tests

--- tests/Config/Compiler/TaskCompilerPassTest.php
+++ tests/Config/Compiler/TaskCompilerPassTest.php
@@ -1,8 +1,11 @@
 <?php
 
-namespace ClickNow\Checker\Config;
+namespace ClickNow\Checker\Config\Compiler;
 
 use Mockery as m;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 /**
  * @group config/compiler
@@ -10,15 +13,59 @@ use Mockery as m;
  */
 class TaskCompilerPassTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerBuilder|\Mockery\MockInterface
+     */
+    protected $container;
+
+    /**
+     * @var \ClickNow\Checker\Config\Compiler\TaskCompilerPass
+     */
+    protected $taskCompilerPass;
+
+    protected function setUp()
+    {
+        $this->container = m::mock(ContainerBuilder::class);
+        $this->container
+            ->shouldReceive('findTaggedServiceIds')
+            ->with(TaskCompilerPass::TAG_TASK)
+            ->atMost()
+            ->once()
+            ->andReturn(['foo' => [['config' => 'foo']]]);
+
+        $this->taskCompilerPass = new TaskCompilerPass();
+    }
+
+    protected function tearDown()
+    {
+        m::close();
+    }
+
+    public function testConstruct()
+    {
+        $this->assertInstanceOf(CompilerPassInterface::class, $this->taskCompilerPass);
+        $this->assertInstanceOf(AbstractCompilerPass::class, $this->taskCompilerPass);
+    }
+
     public function testA()
     {
-        /*$m = \Mockery::mock(ContainerBuilder::class);
-        $m->shouldReceive('getParameter')->with('tasks')->once()->andReturn(['aa' => []]);
-        $m->shouldReceive('findTaggedServiceIds')->with(TaskCompilerPass::TAG_TASK)->once()->andReturn([
-        'a' => [['config' => 'a']]
-        ]);
+        $this->container->shouldReceive('getParameter')->with('tasks')->once()->andReturn(['foo' => []]);
 
-        $a = new TaskCompilerPas*/
+        $definition = m::mock(Definition::class);
+        $definition->shouldReceive('addMethodCall')->with('mergeDefaultConfig', [[]])->once()->andReturn();
 
+        $this->container->shouldReceive('findDefinition')->with('foo')->once()->andReturn($definition);
+        $this->taskCompilerPass->process($this->container);
+    }
+
+    public function testAA()
+    {
+        $this->container->shouldReceive('getParameter')->with('tasks')->once()->andReturn(['foo' => []]);
+
+        $definition = m::mock(Definition::class);
+        $definition->shouldReceive('addMethodCall')->with('mergeDefaultConfig', [[]])->once()->andReturn();
+
+        $this->container->shouldReceive('findDefinition')->with('foo')->once()->andReturn($definition);
+        $this->taskCompilerPass->process($this->container);
     }
 }
