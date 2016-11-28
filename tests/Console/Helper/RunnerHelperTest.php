814ba29a706512d20848fcbf4425459e67ce5b71
Ricardo Gobbo <ricardo@clicknow.com.br>
2016-11-28 16:37:53 -0200

    Add more tests

--- tests/Console/Helper/RunnerHelperTest.php
+++ tests/Console/Helper/RunnerHelperTest.php
@@ -2,15 +2,17 @@
 
 namespace ClickNow\Checker\Console\Helper;
 
+use ClickNow\Checker\Action\ActionInterface;
 use ClickNow\Checker\Action\ActionsCollection;
 use ClickNow\Checker\Command\CommandInterface;
 use ClickNow\Checker\Context\ContextInterface;
+use ClickNow\Checker\Event\ActionEvent;
 use ClickNow\Checker\Event\RunnerEvent;
 use ClickNow\Checker\IO\IOInterface;
+use ClickNow\Checker\Result\Result;
 use Mockery as m;
 use Symfony\Component\Console\Helper\Helper;
 use Symfony\Component\Console\Helper\HelperInterface;
-use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
@@ -34,11 +36,35 @@ class RunnerHelperTest extends \PHPUnit_Framework_TestCase
      */
     protected $runnerHelper;
 
+    /**
+     * @var \ClickNow\Checker\Context\ContextInterface|\Mockery\MockInterface
+     */
+    protected $context;
+
+    /**
+     * @var \ClickNow\Checker\Command\CommandInterface|\Mockery\MockInterface
+     */
+    protected $command;
+
+    /**
+     * @var \ClickNow\Checker\Action\ActionsCollection
+     */
+    protected $actions;
+
     protected function setUp()
     {
         $this->dispatcher = m::mock(EventDispatcherInterface::class);
         $this->io = m::mock(IOInterface::class);
         $this->runnerHelper = new RunnerHelper($this->dispatcher, $this->io);
+        $this->context = m::mock(ContextInterface::class);
+        $this->command = m::mock(CommandInterface::class);
+        $this->actions = new ActionsCollection();
+
+        $this->io->shouldReceive('title')->with('/`foo`/')->atMost()->once()->andReturnNull();
+
+        $this->context->shouldReceive('getCommand')->withNoArgs()->andReturn($this->command);
+        $this->command->shouldReceive('getName')->withNoArgs()->andReturn('foo');
+        $this->command->shouldReceive('getActionsToRun')->with($this->context)->andReturn($this->actions);
     }
 
     protected function tearDown()
@@ -59,19 +85,101 @@ class RunnerHelperTest extends \PHPUnit_Framework_TestCase
 
     public function testRunWithoutActions()
     {
-        $context = m::mock(ContextInterface::class);
-        $command = m::mock(CommandInterface::class);
+        $this->dispatcher
+            ->shouldReceive('dispatch')
+            ->with(RunnerEvent::RUNNER_RUN, m::type(RunnerEvent::class))
+            ->never();
 
-        $context->shouldReceive('getCommand')->withNoArgs()->once()->andReturn($command);
-        $command->shouldReceive('getName')->withNoArgs()->once()->andReturn('foo');
-        $command->shouldReceive('getActionsToRun')->with($context)->once()->andReturn(new ActionsCollection());
-
-        $this->dispatcher->shouldReceive('dispatch')->with(RunnerEvent::RUNNER_RUN, m::type(Event::class))->never();
-
-        $this->io->shouldReceive('title')->with('/`foo`/')->once()->andReturnNull();
         $this->io->shouldReceive('note')->with('No actions available.')->once()->andReturnNull();
 
-        $result = $this->runnerHelper->run($context);
-        $this->assertSame(0, $result);
+        $this->assertSame(0, $this->runnerHelper->run($this->context));
+    }
+
+    public function testRunAndReturnCodeSuccess()
+    {
+        $this->createAction('action1', Result::SUCCESS);
+        $this->createAction('action2', Result::WARNING);
+        $this->createAction('action3', Result::SKIPPED);
+
+        $runnerType = m::type(RunnerEvent::class);
+
+        $this->dispatcher->shouldReceive('dispatch')->with(RunnerEvent::RUNNER_RUN, $runnerType)->once();
+        $this->dispatcher->shouldReceive('dispatch')->with(RunnerEvent::RUNNER_FAILED, $runnerType)->never();
+        $this->dispatcher->shouldReceive('dispatch')->with(RunnerEvent::RUNNER_SUCCESSFULLY, $runnerType)->once();
+
+        $this->assertSame(0, $this->runnerHelper->run($this->context));
+    }
+
+    public function testRunAndReturnCodeError()
+    {
+        $this->command->shouldReceive('isStopOnFailure')->withNoArgs()->andReturn(false);
+
+        $this->createAction('action1', Result::SUCCESS);
+        $this->createAction('action2', Result::WARNING);
+        $this->createAction('action3', Result::SKIPPED);
+        $this->createAction('action4', Result::ERROR);
+
+        $runnerType = m::type(RunnerEvent::class);
+
+        $this->dispatcher->shouldReceive('dispatch')->with(RunnerEvent::RUNNER_RUN, $runnerType)->once();
+        $this->dispatcher->shouldReceive('dispatch')->with(RunnerEvent::RUNNER_FAILED, $runnerType)->once();
+        $this->dispatcher->shouldReceive('dispatch')->with(RunnerEvent::RUNNER_SUCCESSFULLY, $runnerType)->never();
+
+        $this->assertSame(1, $this->runnerHelper->run($this->context));
+    }
+
+    public function testRunWithStopOnFailure()
+    {
+        $this->command->shouldReceive('isStopOnFailure')->withNoArgs()->andReturn(true);
+
+        $this->createAction('action1', Result::ERROR);
+        $this->createAction('action2', Result::ERROR, 0);
+
+        $runnerType = m::type(RunnerEvent::class);
+
+        $this->dispatcher->shouldReceive('dispatch')->with(RunnerEvent::RUNNER_RUN, $runnerType)->once();
+        $this->dispatcher->shouldReceive('dispatch')->with(RunnerEvent::RUNNER_FAILED, $runnerType)->once();
+        $this->dispatcher->shouldReceive('dispatch')->with(RunnerEvent::RUNNER_SUCCESSFULLY, $runnerType)->never();
+
+        $this->assertSame(1, $this->runnerHelper->run($this->context));
+    }
+
+    /**
+     * Create action.
+     *
+     * @param string $name
+     * @param int    $status
+     * @param int    $times
+     *
+     * @return void
+     */
+    protected function createAction($name, $status, $times = 1)
+    {
+        $action = m::mock(ActionInterface::class);
+        $action->shouldReceive('getName')->withNoArgs()->times($times)->andReturn($name);
+
+        $result = new Result($status, $this->command, $this->context, $action, null);
+
+        $this->actions->add($action);
+        $this->io->shouldReceive('log')->with('/`'.$name.'`/')->times($times)->andReturnNull();
+        $this->command->shouldReceive('runAction')->with($this->context, $action)->times($times)->andReturn($result);
+
+        $this->dispatcher
+            ->shouldReceive('dispatch')
+            ->with(ActionEvent::ACTION_RUN, m::type(ActionEvent::class))
+            ->times($times)
+            ->andReturn(m::mock(ActionEvent::class));
+
+        $this->dispatcher
+            ->shouldReceive('dispatch')
+            ->with(ActionEvent::ACTION_FAILED, m::type(ActionEvent::class))
+            ->times($result->isSuccess() ? 0 : $times)
+            ->andReturn(m::mock(ActionEvent::class));
+
+        $this->dispatcher
+            ->shouldReceive('dispatch')
+            ->with(ActionEvent::ACTION_SUCCESSFULLY, m::type(ActionEvent::class))
+            ->times($result->isSuccess() ? $times : 0)
+            ->andReturn(m::mock(ActionEvent::class));
     }
 }
