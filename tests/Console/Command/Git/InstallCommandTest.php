84ec5cdf3bfe0b9ce84cc106f2b8d1758230a1e2
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2016-12-13 10:54:13 -0200

    Formatting

--- tests/Console/Command/Git/InstallCommandTest.php
+++ tests/Console/Command/Git/InstallCommandTest.php
@@ -13,7 +13,7 @@ use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\ProcessBuilder;
 
 /**
- * @group console/commandi
+ * @group console/command
  * @covers \ClickNow\Checker\Console\Command\Git\InstallCommand
  */
 class InstallCommandTest extends \PHPUnit_Framework_TestCase
@@ -68,7 +68,14 @@ class InstallCommandTest extends \PHPUnit_Framework_TestCase
         $app = new Application();
         $app->add(new InstallCommand($this->checker, $this->filesystem, $this->io, $this->processBuilder));
 
+        $this->checker->shouldReceive('getHooksPreset')->withNoArgs()->andReturn($this->tmpDir);
+        $this->checker->shouldReceive('getHooksDir')->withNoArgs()->andReturn(null)->byDefault();
+
         $this->pathsHelper = m::spy(PathsHelper::class);
+        $this->pathsHelper->shouldReceive('getGitHooksDir')->withNoArgs()->once()->andReturn($this->tmpDir);
+        $this->pathsHelper->shouldReceive('getGitHookTemplatesDir')->withNoArgs()->andReturn('');
+        $this->pathsHelper->shouldReceive('getPathWithTrailingSlash')->with($this->tmpDir)->andReturn($this->tmpDir);
+        $this->pathsHelper->shouldReceive('getPathWithTrailingSlash')->with(null)->andReturn(false);
 
         $command = $app->find('git:install');
         $command->getHelperSet()->set($this->pathsHelper, 'paths');
@@ -86,33 +93,30 @@ class InstallCommandTest extends \PHPUnit_Framework_TestCase
 
     public function testRun()
     {
-        $dir = $this->tmpDir;
+        file_put_contents($this->tmpDir.'all', '');
+        file_put_contents($this->tmpDir.'pre-commit', '');
+        file_put_contents($this->tmpDir.'pre-push', InstallCommand::GENERATED_MESSAGE);
 
-        file_put_contents($dir.'all', '');
-        file_put_contents($dir.'pre-commit', '');
-        file_put_contents($dir.'pre-push', InstallCommand::GENERATED_MESSAGE);
+        $this->checker->shouldReceive('getHooksDir')->withNoArgs()->andReturnValues([null, $this->tmpDir]);
 
-        $this->checker->shouldReceive('getHooksPreset')->withNoArgs()->andReturn($dir);
-        $this->checker->shouldReceive('getHooksDir')->withNoArgs()->andReturnValues([null, $dir]);
-
-        $this->pathsHelper->shouldReceive('getGitHooksDir')->withNoArgs()->once()->andReturn($dir);
-        $this->pathsHelper->shouldReceive('getGitHookTemplatesDir')->withNoArgs()->andReturn('');
-        $this->pathsHelper->shouldReceive('getPathWithTrailingSlash')->with($dir)->andReturn($dir);
-        $this->pathsHelper->shouldReceive('getPathWithTrailingSlash')->with(null)->andReturn(false);
         $this->pathsHelper->shouldReceive('getAbsolutePath')->with('foo')->andReturnValues(['foo', 'bar']);
         $this->pathsHelper->shouldReceive('getDefaultConfigPath')->withNoArgs()->andReturn('bar');
         $this->pathsHelper->shouldReceive('getRelativeProjectPath')->with('foo')->once()->andReturn('foo');
 
-        $this->filesystem->shouldReceive('exists')->with($dir)->once()->andReturn(false);
-        $this->filesystem->shouldReceive('mkdir')->with($dir)->once()->andReturnNull();
-
-        $this->filesystem->shouldReceive('exists')->with($dir.'all')->andReturn(true);
-        $this->filesystem->shouldReceive('exists')->with($dir.'pre-commit')->andReturn(true);
-        $this->filesystem->shouldReceive('exists')->with($dir.'pre-push')->andReturn(true);
+        $this->filesystem->shouldReceive('exists')->with($this->tmpDir)->once()->andReturn(false);
+        $this->filesystem->shouldReceive('mkdir')->with($this->tmpDir)->once()->andReturnNull();
+        $this->filesystem->shouldReceive('exists')->with($this->tmpDir.'all')->andReturn(true);
+        $this->filesystem->shouldReceive('exists')->with($this->tmpDir.'pre-commit')->andReturn(true);
+        $this->filesystem->shouldReceive('exists')->with($this->tmpDir.'pre-push')->andReturn(true);
         $this->filesystem->shouldReceive('exists')->withAnyArgs()->andReturn(false);
-        $this->filesystem->shouldReceive('rename')->with($dir.'pre-commit', $dir.'pre-commit.checker', true)->once();
         $this->filesystem->shouldReceive('dumpFile')->withAnyArgs()->andReturnNull();
         $this->filesystem->shouldReceive('chmod')->withAnyArgs()->andReturnNull();
+
+        $this->filesystem
+            ->shouldReceive('rename')
+            ->with($this->tmpDir.'pre-commit', $this->tmpDir.'pre-commit.checker', true)
+            ->once()
+            ->andReturnNull();
 
         $this->io->shouldReceive('title')->withAnyArgs()->once()->andReturnNull();
         $this->io->shouldReceive('log')->withAnyArgs()->andReturnNull();
@@ -134,18 +138,8 @@ class InstallCommandTest extends \PHPUnit_Framework_TestCase
     {
         $this->setExpectedException(FileNotFoundException::class);
 
-        $dir = $this->tmpDir;
-
-        $this->checker->shouldReceive('getHooksPreset')->withNoArgs()->once()->andReturn($dir);
-        $this->checker->shouldReceive('getHooksDir')->withNoArgs()->once()->andReturn(null);
-
-        $this->pathsHelper->shouldReceive('getGitHooksDir')->withNoArgs()->once()->andReturn($dir);
-        $this->pathsHelper->shouldReceive('getGitHookTemplatesDir')->withNoArgs()->once()->andReturn('');
-        $this->pathsHelper->shouldReceive('getPathWithTrailingSlash')->with($dir)->once()->andReturn($dir);
-        $this->pathsHelper->shouldReceive('getPathWithTrailingSlash')->with(null)->once()->andReturn(false);
-
-        $this->filesystem->shouldReceive('exists')->with($dir)->once()->andReturn(true);
-        $this->filesystem->shouldReceive('exists')->with(m::not($dir))->twice()->andReturn(false);
+        $this->filesystem->shouldReceive('exists')->with($this->tmpDir)->once()->andReturn(true);
+        $this->filesystem->shouldReceive('exists')->with(m::not($this->tmpDir))->twice()->andReturn(false);
 
         $this->io->shouldReceive('title')->withAnyArgs()->once()->andReturnNull();
         $this->io->shouldReceive('success')->withAnyArgs()->never();
